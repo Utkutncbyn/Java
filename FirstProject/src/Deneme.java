d0855338ab4d315447a7d11b6603f62b5b46a407
Utku Tuncabayın <112210779+Utkutncbyn@users.noreply.github.com>
2023-08-30 16:02:20 +0300

    Add files via upload

---
+++ FirstProject/src/Deneme.java
@@ -0,0 +1,13 @@
+public class Deneme {
+
+    public static void main (String[] args) {
+
+        System.out.println("Merhaba Java");
+
+        System.out.print("Aynı Satıra Yazmak İçin !!!");
+
+        System.out.println("Hoşgeldiniz...");
+
+    }
+
+}
